f20123ba1f0f48510f5bead2e448921342ef82f5
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-18 10:15:20 -0400

    Initial Structure Complete
    Set up the form and input/buttons along with the display area for the paragraph text.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("working");
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of the same old Ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleClick}>
+        <label htmlFor="amount">paragraphs</label>
+        <input
+          type="number"
+          name="amount"
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default App;
